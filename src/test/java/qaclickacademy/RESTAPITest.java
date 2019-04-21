0ba7f65e93e5bba99043444f042233490945dc4e
sachin <sy04071990@gmail.com>
2019-04-21 15:30:48 +0530

    this is for ist timezone

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -7,7 +7,9 @@ public class RESTAPITest {
 	@Test
 	public void postJira()
 	{
-	System.out.println("postJira");	
+	System.out.println("postJira");
+	System.out.println("postjira2");
+	System.out.println("postjira3");
 	}
   @Test
   public void deleteTwitter()
