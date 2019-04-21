26459dc591b5e1e9a98c53b2967ed584f3a71482
sachin <sy04071990@gmail.com>
2019-04-21 15:00:25 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+import org.testng.annotations.Test;
+public class AppiumTest {
+
+	
+	@Test
+	public void NAtiveAppAndroid()
+	{
+		System.out.println("NativeApp");
+	}
+  @Test
+  public void IOsApps()
+  {
+	System.out.println("IOsApp");  
+  }
+	
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+	System.out.println("BrowerAutomation");	
+	}
+	@Test
+	public void elementsUi()
+	{
+	System.out.println("elementsUi");	
+	}
+}
